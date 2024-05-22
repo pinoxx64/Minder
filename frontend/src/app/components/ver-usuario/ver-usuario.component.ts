d24464c3f50fa76eebcdc1cd1b05bb4ca120e2bb
Raul GM <loscanos3raul@gmail.com>
2024-05-23 00:30:33 +0200

    crear ver usuarios

--- frontend/src/app/components/ver-usuario/ver-usuario.component.ts
+++ frontend/src/app/components/ver-usuario/ver-usuario.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { EditableColumn, TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { CrearUsuarioComponent } from '../crear-usuario/crear-usuario.component';
+import { EditarUsuarioComponent } from '../editar-usuario/editar-usuario.component';
+
+import { Usuario } from '../../interface/usuario';
+import { UsuarioService } from '../../service/usuario.service';
 
 @Component({
   selector: 'app-ver-usuario',
   standalone: true,
-  imports: [],
+  imports: [
+    HttpClientModule,
+    RouterLink,
+    TableModule,
+    ButtonModule,
+    CrearUsuarioComponent,
+    EditarUsuarioComponent  
+  ],
   templateUrl: './ver-usuario.component.html',
-  styleUrl: './ver-usuario.component.css'
+  styleUrl: './ver-usuario.component.css',
+  providers:[
+    UsuarioService
+  ]
 })
 export class VerUsuarioComponent {
+  constructor(
+    private servicioUsuario: UsuarioService
+  ){}
+  subscriptionUsers: Subscription=new Subscription;
+  listaUsers:Array<Usuario>=[]
+  @Input() admin=true
 
+  ngOnInit(): void{
+    this.subscriptionUsers = this.servicioUsuario.usuariosGet().subscribe({
+      next: (data: Array<Usuario>) => {
+        this.listaUsers=data
+      },
+      error: (e) => {
+
+      }
+    })
+  }
 }
