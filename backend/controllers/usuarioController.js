dd2ebffcf980c04696b6c1a1bf7a3587e4d9ccb7
Raul GM <loscanos3raul@gmail.com>
2024-05-27 18:24:14 +0200

    preparecion para crear el login y el registro en frontend

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,5 +1,5 @@
 const {response,request} = require('express');
-const Conexion = require('../database/ConexionUsuario')
+const Conexion = require('../database/conexionUsuario')
 
 const usuariosGet =  (req, res = response) => {
     const conx = new Conexion();
