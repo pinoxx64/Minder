d891f8c4b0c2109c50620c97722c325775162392
Raul GM <loscanos3raul@gmail.com>
2024-05-29 12:29:52 +0200

    apps

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
+import { EventoComponent } from './components/evento/evento.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ButtonModule,RippleModule],
+  imports: [ButtonModule,RippleModule,RouterOutlet,RouterLink,CommonModule,EventoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
